e0057eac25422d85ee46a91e74094b59cf54bd22
amir <biktagirov.akh@phystech.edu>
2024-03-12 18:55:02 +0300

    lol

--- ciletters/letter.go
+++ ciletters/letter.go
@@ -2,6 +2,31 @@
 
 package ciletters
 
+import (
+	_ "embed"
+	"strings"
+	"text/template"
+)
+
+func LastRows(log string) []string {
+	answer := strings.Split(log, "\n")
+	return answer[max(0, len(answer)-10):]
+}
+
+//go:embed lolkekcheburek.txt
+var ff string
+
 func MakeLetter(n *Notification) (string, error) {
-	return "", nil
+	tmpl := template.New("test")
+	tmpl = tmpl.Funcs(template.FuncMap{
+		"last_rows": LastRows,
+	})
+	var err error
+	tmpl, err = tmpl.Parse(ff)
+	if err != nil {
+		return "", err
+	}
+	answer := strings.Builder{}
+	err = tmpl.Execute(&answer, n)
+	return answer.String(), err
 }
