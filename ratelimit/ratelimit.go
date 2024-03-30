c16cc4600bd1a2df933ffc8b12e192a93f05589b
amir <biktagirov.akh@phystech.edu>
2024-03-30 15:47:53 +0300

    trash

--- ratelimit/ratelimit.go
+++ ratelimit/ratelimit.go
@@ -10,6 +10,10 @@ import (
 
 // Limiter is precise rate limiter with context support.
 type Limiter struct {
+	stop      bool
+	stopped   chan struct{}
+	free      chan struct{}
+	sleepTime time.Duration
 }
 
 var ErrStopped = errors.New("limiter stopped")
@@ -17,13 +21,37 @@ var ErrStopped = errors.New("limiter stopped")
 // NewLimiter returns limiter that throttles rate of successful Acquire() calls
 // to maxSize events at any given interval.
 func NewLimiter(maxCount int, interval time.Duration) *Limiter {
-	panic("not implemented")
+	answer := Limiter{stop: false, stopped: make(chan struct{}), free: make(chan struct{}, maxCount), sleepTime: interval}
+	for i := 0; i < maxCount; i++ {
+		answer.free <- struct{}{}
+	}
+	return &answer
 }
 
 func (l *Limiter) Acquire(ctx context.Context) error {
-	panic("not implemented")
+	if l.stop {
+		return ErrStopped
+	}
+	select {
+	case <-l.free:
+		go func() {
+			if l.sleepTime != 0 {
+				ticker := time.NewTicker(l.sleepTime)
+				defer ticker.Stop()
+				select {
+				case <-ticker.C:
+				case <-l.stopped:
+				}
+			}
+			l.free <- struct{}{}
+		}()
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+	return nil
 }
 
 func (l *Limiter) Stop() {
-	panic("not implemented")
+	l.stop = true
+	close(l.stopped)
 }
