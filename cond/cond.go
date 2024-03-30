130ab8ee996733e3b71b168d1981d6db9a11a3b8
amir <biktagirov.akh@phystech.edu>
2024-03-30 15:47:02 +0300

    trash

--- cond/cond.go
+++ cond/cond.go
@@ -16,12 +16,16 @@ type Locker interface {
 // which must be held when changing the condition and
 // when calling the Wait method.
 type Cond struct {
-	L Locker
+	L       Locker
+	free    chan struct{}
+	counter chan int
 }
 
 // New returns a new Cond with Locker l.
 func New(l Locker) *Cond {
-	return &Cond{L: l}
+	answer := Cond{L: l, free: make(chan struct{}, 1), counter: make(chan int, 1)}
+	answer.counter <- 0
+	return &answer
 }
 
 // Wait atomically unlocks c.L and suspends execution
@@ -33,15 +37,18 @@ func New(l Locker) *Cond {
 // typically cannot assume that the condition is true when
 // Wait returns. Instead, the caller should Wait in a loop:
 //
-//    c.L.Lock()
-//    for !condition() {
-//        c.Wait()
-//    }
-//    ... make use of condition ...
-//    c.L.Unlock()
-//
+//	c.L.Lock()
+//	for !condition() {
+//	    c.Wait()
+//	}
+//	... make use of condition ...
+//	c.L.Unlock()
 func (c *Cond) Wait() {
-
+	c.L.Unlock()
+	count := <-c.counter
+	c.counter <- count + 1
+	<-c.free
+	c.L.Lock()
 }
 
 // Signal wakes one goroutine waiting on c, if there is any.
@@ -49,7 +56,9 @@ func (c *Cond) Wait() {
 // It is allowed but not required for the caller to hold c.L
 // during the call.
 func (c *Cond) Signal() {
-
+	count := <-c.counter
+	c.counter <- count - 1
+	c.free <- struct{}{}
 }
 
 // Broadcast wakes all goroutines waiting on c.
@@ -57,5 +66,9 @@ func (c *Cond) Signal() {
 // It is allowed but not required for the caller to hold c.L
 // during the call.
 func (c *Cond) Broadcast() {
-
+	for count := <-c.counter; count > 0; count = <-c.counter {
+		c.counter <- count - 1
+		c.free <- struct{}{}
+	}
+	c.counter <- 0
 }
