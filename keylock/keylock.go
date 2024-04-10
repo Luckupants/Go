5ba39f41aee0e9ebe898d60b9131f5b81dbe25fc
amir <biktagirov.akh@phystech.edu>
2024-04-11 00:38:16 +0300

    New changelist

--- keylock/keylock.go
+++ keylock/keylock.go
@@ -2,12 +2,45 @@
 
 package keylock
 
-type KeyLock struct{}
+import (
+	"sort"
+	"sync"
+)
+
+type KeyLock struct {
+	mutexes map[string]chan struct{}
+	mx      sync.Mutex
+}
 
 func New() *KeyLock {
-	panic("implement me")
+	return &KeyLock{mutexes: make(map[string]chan struct{})}
 }
 
 func (l *KeyLock) LockKeys(keys []string, cancel <-chan struct{}) (canceled bool, unlock func()) {
-	panic("implement me")
+	copyKeys := make([]string, len(keys))
+	copy(copyKeys, keys)
+	sort.Strings(copyKeys)
+	keys = copyKeys
+	unlock = func() {
+		l.mx.Lock()
+		for i := len(keys) - 1; i >= 0; i-- {
+			l.mutexes[keys[i]] <- struct{}{}
+		}
+		l.mx.Unlock()
+	}
+	for _, key := range keys {
+		l.mx.Lock()
+		if _, ok := l.mutexes[key]; !ok {
+			l.mutexes[key] = make(chan struct{}, 1)
+			l.mutexes[key] <- struct{}{}
+		}
+		mutex := l.mutexes[key]
+		l.mx.Unlock()
+		select {
+		case <-mutex:
+		case <-cancel:
+			return true, unlock
+		}
+	}
+	return false, unlock
 }
