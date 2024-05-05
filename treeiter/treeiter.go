82aaeba26888f0eac41f2a1c36cdb04a09d2275c
amir <biktagirov.akh@phystech.edu>
2024-05-05 19:54:31 +0300

    lol

--- treeiter/treeiter.go
+++ treeiter/treeiter.go
@@ -1,3 +1,20 @@
 //go:build !solution
 
 package treeiter
+
+type Node interface {
+	Left() *Node
+	Right() *Node
+}
+
+func DoInOrder[Noda interface {
+	Left() *Noda
+	Right() *Noda
+}](root *Noda, cb func(val *Noda)) {
+	if root == nil {
+		return
+	}
+	DoInOrder((*root).Left(), cb)
+	cb(root)
+	DoInOrder((*root).Right(), cb)
+}
