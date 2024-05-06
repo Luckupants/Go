73eb1ba8bcbf10fefbf30517f26a2e545b0a1448
amir <biktagirov.akh@phystech.edu>
2024-05-06 20:30:26 +0300

    lol

--- reversemap/reversemap.go
+++ reversemap/reversemap.go
@@ -2,6 +2,19 @@
 
 package reversemap
 
-func ReverseMap(forward interface{}) interface{} {
-	panic("implement me")
+import (
+	"reflect"
+)
+
+func ReverseMap(mapa any) any {
+	m := reflect.TypeOf(mapa)
+	var newMapType = reflect.MapOf(m.Elem(), m.Key())
+	newMap := reflect.MakeMapWithSize(newMapType, 0)
+	iter := reflect.ValueOf(mapa).MapRange()
+	for iter.Next() {
+		k := iter.Key()
+		v := iter.Value()
+		newMap.SetMapIndex(v, k)
+	}
+	return newMap.Interface()
 }
