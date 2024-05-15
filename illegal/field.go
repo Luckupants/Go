90e8843f21a5a7d5dcfd2594696babc3038b9bfd
amir <biktagirov.akh@phystech.edu>
2024-05-16 00:17:55 +0300

    lol

--- illegal/field.go
+++ illegal/field.go
@@ -2,5 +2,18 @@
 
 package illegal
 
+import (
+	"reflect"
+	"unsafe"
+)
+
 func SetPrivateField(obj interface{}, name string, value interface{}) {
+	v := reflect.ValueOf(obj)
+	f, has := reflect.TypeOf(obj).Elem().FieldByName(name)
+	if !has || f.Type != reflect.TypeOf(value) {
+		panic("ti daun??")
+	}
+	p := unsafe.Pointer((uintptr)(v.UnsafePointer()) + f.Offset)
+	vv := reflect.NewAt(reflect.TypeOf(value), p)
+	vv.Elem().Set(reflect.ValueOf(value))
 }
